2b19b0324f352bbe861fdb681cf4021b3024c896
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 11:48:33 +0100

    Fix ambiguous library name

--- module.cmake
+++ module.cmake
@@ -6,8 +6,8 @@ endif()
 
 include_directories(${MODULE_MATH_DIR}/include)
 
-add_library(aurora-math SHARED ${MODULE_MATH_SOURCE_DIR}/Vector2D.cpp
+add_library(aurorafw-math SHARED ${MODULE_MATH_SOURCE_DIR}/Vector2D.cpp
                                ${MODULE_MATH_SOURCE_DIR}/Vector3D.cpp
                                ${MODULE_MATH_SOURCE_DIR}/Vector4D.cpp)
 
-set_target_properties(aurora-math PROPERTIES OUTPUT_NAME aurora-math)
+set_target_properties(aurorafw-math PROPERTIES OUTPUT_NAME aurorafw-math)
