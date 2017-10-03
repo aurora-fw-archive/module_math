89cc9fd960ad4db439da3d4a56a7421a730fad60
Luís Ferreira <lsferreira169@gmail.com>
2017-10-03 19:35:25 +0100

    Improve declaration of Vector4D

--- include/AuroraFW/Math/Vector4D.h
+++ include/AuroraFW/Math/Vector4D.h
@@ -25,42 +25,83 @@
 
 namespace AuroraFW {
 	namespace Math {
-		template<class T> struct vec3;
+		template<typename T> struct vec3;
 
-		template<class T>
+		/**
+		 * A struct that represents a 4D vector. A struct that store's
+		 * position in 4D coordinates, allows to manipulate them and also
+		 * to do vector operations.
+		 * @since snapshot20171003
+		 */
+		template<typename T>
 		struct AFW_PREFIX vec4 {
 			vec4();
 			vec4(const T& );
 			vec4(const T& , const T& , const T& , const T& );
 			vec4(const vec3<T>& , const T& );
 
-			vec4<T>& Add(const vec4<T>& );
-			vec4<T>& Subtract(const vec4<T>& );
-			vec4<T>& Multiply(const vec4<T>& );
-			vec4<T>& Divide(const vec4<T>& );
+			vec4<T>& add(const vec4<T>& );
+			vec4<T>& add(const T& , const T& , const T& , const T& );
+			vec4<T>& subtract(const vec4<T>& );
+			vec4<T>& subtract(const T& , const T& , const T& , const T& );
+			vec4<T>& multiply(const vec4<T>& );
+			vec4<T>& multiply(const T& , const T& , const T& , const T& );
+			vec4<T>& divide(const vec4<T>& );
+			vec4<T>& divide(const T& , const T& , const T& , const T& );
 
-			template<typename t>
-			friend vec4<T> operator+(vec4<T> , const vec4<T>& );
-			template<typename t>
-			friend vec4<T> operator-(vec4<T> , const vec4<T>& );
-			template<typename t>
-			friend vec4<T> operator*(vec4<T> , const vec4<T>& );
-			template<typename t>
-			friend vec4<T> operator/(vec4<T> , const vec4<T>& );
+			void setX(T );
+			void setY(T );
+			void setZ(T );
+			void setW(T );
+
+			void normalize();
 
 			bool operator==(const vec4<T>& ) const;
 			bool operator!=(const vec4<T>& ) const;
+			bool operator<(const vec3<T>& ) const;
+			bool operator>(const vec3<T>& ) const;
+			bool operator<=(const vec3<T>& ) const;
+			bool operator>=(const vec3<T>& ) const;
+
+			vec4<T> operator+(const vec4<T>& );
+			vec4<T> operator+(const T& );
+			vec4<T> operator-(const vec4<T>& );
+			vec4<T> operator-(const T& );
+			vec4<T> operator*(const vec4<T>& );
+			vec4<T> operator*(const T& );
+			vec4<T> operator/(const vec4<T>& );
+			vec4<T> operator/(const T& );
 
 			vec4<T>& operator+=(const vec4<T>& );
+			vec4<T>& operator+=(const T& );
 			vec4<T>& operator-=(const vec4<T>& );
+			vec4<T>& operator-=(const T& );
 			vec4<T>& operator*=(const vec4<T>& );
+			vec4<T>& operator*=(const T& );
 			vec4<T>& operator/=(const vec4<T>& );
+			vec4<T>& operator/=(const T& );
+
+			T getX() const;
+			T getY() const;
+			T getZ() const;
+			T getW() const;
+
+			T length() const;
+			T magnitude() const;
+			bool isNull() const;
+			vec4<T> normalized() const;
+			T dot(vec4<T>) const;
+			T distanceToPoint(const vec4<T>& ) const;
+			T distanceToLine(const vec4<T>&, const vec4<T>& ) const;
+			std::string toString() const;
 
 			template<typename t>
 			friend std::ostream& operator<<(std::ostream& , const vec4<T>& );
 
 			T x, y, z, w;
 		};
+
+		typedef vec4<float> Vector4D;
 	}
 }
 
